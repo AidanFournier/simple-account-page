e83d17b67bce794cdba404310ae28dbe8ae494ff
Aidan Fournier <aidan.douglas.fournier@gmail.com>
2022-02-24 22:53:27 +0900

    navbar cleaned up

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'pages#home'
+  root to: 'pages#profile'
 
-  get 'profile',  to: 'pages#profile'
+  # get 'profile',  to: 'pages#profile'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
