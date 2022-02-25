02a66e4f5c563a1ab70e320ce4477434dde34e78
Aidan Fournier <aidan.douglas.fournier@gmail.com>
2022-02-25 19:39:22 +0900

    Styled profile page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,33 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :profile ]
+  skip_before_action :authenticate_user!, only: [ :profile, :update ]
 
-  def profile
+  before_action :set_user, only: %i[ show edit update]
+
+  def home
+  end
+
+  def show
   end 
+
+  def edit
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+     redirect_to profile_path
+    else
+     render "new"
+    end
+  end
+end
+
+private
+
+def set_user
+  @user = User.find(params[:id])
+end 
+
+def user_params
+  params.require(:user).permit(:first_name, :last_name, :email, :password)
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'pages#profile'
+  root to: 'pages#home'
+  get 'profile',  to: 'pages#profile'
 
-  resources :users, only: [ :update ]
-  # get 'profile',  to: 'pages#profile'
+  resources :users, only: [ :show, :edit, :update ]
+  
 end
